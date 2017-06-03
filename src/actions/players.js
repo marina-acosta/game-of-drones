632866fd54f57816de247e20c52e9642f872e3e6
Marina Alicia Acosta Pedemonte <macosta@synchronit.com>
2017-06-03 18:54:07 -0300

    Remove unused code

--- src/actions/players.js
+++ src/actions/players.js
@@ -1,5 +1,4 @@
 export const SET_PLAYERS = 'SET_PLAYERS'
-export const RESET_PLAYERS = 'RESET_PLAYERS'
 
 export function setPlayersAction(one, two) {
     return {
@@ -11,13 +10,5 @@ export function setPlayersAction(one, two) {
 export function setPlayers(one, two){
     return dispatch => {
         dispatch(setPlayersAction(one, two))
-    }
-}
-
-export function resetPlayers() {
-    return dispatch => {
-        dispatch({
-            type: RESET_PLAYERS
-        })
     }
 }
